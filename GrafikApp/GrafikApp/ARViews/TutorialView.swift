14285ccd11bf6338a69c4f8323070a8fcddbfb88
Rossana <rsilva.rossana@gmail.com>
2025-04-18 22:17:49 -0300

    Adiciona navegação para página de tutorial

---
+++ GrafikApp/GrafikApp/ARViews/TutorialView.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialView.swift
+//  GrafikApp
+//
+//  Created by Rossana Rocha on 18/04/25.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text("Página de Tutorial (em construção)")
+            .font(.balooRegular(size: 22))
+            .padding()
+    }
+}
+
--- GrafikApp/GrafikApp/HomeView.swift
+++ GrafikApp/GrafikApp/HomeView.swift
@@ -13,75 +13,91 @@ struct HomeView: View {
     @State private var isTransformationsARViewPresented = false
 
     var body: some View {
-        VStack(spacing: 20) {
-            Image("Logo Grafik")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 300, height: 200)
+        NavigationView {
+            ZStack {
+                VStack(spacing: 20) {
+                    Image("Logo Grafik")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 200)
 
-            Button(action: {
-                isCameraARViewPresented = true
-            }) {
-                Text("CÂMERA VIRTUAL")
-                    .font(.balooBold(size: 20))
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.grafikBlue)
-                    .cornerRadius(14)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 14)
-                            .stroke(Color.grafikOutline, lineWidth: 3)
-                    )
-                    .padding(.horizontal, 40)
-            }
-            .fullScreenCover(isPresented: $isCameraARViewPresented) {
-                CameraARViewScreen()
-            }
+                    Button(action: {
+                        isCameraARViewPresented = true
+                    }) {
+                        Text("CÂMERA VIRTUAL")
+                            .font(.balooBold(size: 20))
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.grafikBlue)
+                            .cornerRadius(14)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.grafikOutline, lineWidth: 3)
+                            )
+                            .padding(.horizontal, 40)
+                    }
+                    .fullScreenCover(isPresented: $isCameraARViewPresented) {
+                        CameraARViewScreen()
+                    }
 
-            Button(action: {
-                isIlluminationARViewPresented = true
-            }) {
-                Text("ILUMINAÇÃO")
-                    .font(.balooBold(size: 20))
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.grafikRed)
-                    .cornerRadius(14)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 14)
-                            .stroke(Color.grafikOutline, lineWidth: 3)
-                    )
-                    .padding(.horizontal, 40)
-            }
-            .fullScreenCover(isPresented: $isIlluminationARViewPresented) {
-                IlluminationARViewScreen()
-            }
+                    Button(action: {
+                        isIlluminationARViewPresented = true
+                    }) {
+                        Text("ILUMINAÇÃO")
+                            .font(.balooBold(size: 20))
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.grafikRed)
+                            .cornerRadius(14)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.grafikOutline, lineWidth: 3)
+                            )
+                            .padding(.horizontal, 40)
+                    }
+                    .fullScreenCover(isPresented: $isIlluminationARViewPresented) {
+                        IlluminationARViewScreen()
+                    }
 
-            Button(action: {
-                isTransformationsARViewPresented = true
-            }) {
-                Text("TRANSFORMAÇÕES GEOMÉTRICAS")
-                    .font(.balooBold(size: 20))
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.grafikGreen)
-                    .cornerRadius(14)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 14)
-                            .stroke(Color.grafikOutline, lineWidth: 3)
-                    )
-                    .padding(.horizontal, 40)
+                    Button(action: {
+                        isTransformationsARViewPresented = true
+                    }) {
+                        Text("TRANSFORMAÇÕES GEOMÉTRICAS")
+                            .font(.balooBold(size: 20))
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.grafikGreen)
+                            .cornerRadius(14)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.grafikOutline, lineWidth: 3)
+                            )
+                            .padding(.horizontal, 40)
+                    }
+                    .fullScreenCover(isPresented: $isTransformationsARViewPresented) {
+                        TransformationTheoryScreen()
+                    }
+                }
+                .padding(.top, 40)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.grafikBackground)
+                .edgesIgnoringSafeArea(.all)
             }
-            .fullScreenCover(isPresented: $isTransformationsARViewPresented) {
-                TransformationTheoryScreen()
-            }
+            .overlay(
+                NavigationLink(destination: TutorialView()) {
+                    Image(systemName: "questionmark.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.grafikOutline)
+                        .padding(.top, 16)
+                        .padding(.trailing, 16)
+                },
+                alignment: .topTrailing
+            )
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.grafikBackground)
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
